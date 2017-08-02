65e780465c09d3781539e55808419b47a952f024
Chalo Salvador <chalosalvador@gmail.com>
2017-08-02 09:12:55 -0500

    ejercicio

---
+++ tienda.java
@@ -0,0 +1,16 @@
+/*
+Escribir un programa que permita registrar clientes en una tienda.
+El programa debe permitir un usuario administrador que se encargará de manejar los productos
+que se venden en la tienda.
+Los productos pueden ser de tres categorias:
+    alimentos: nombre, precio.
+    electrónicos: nombre, precio, marca, observaciones
+    salud: nombre, precio, venta libre, fecha expiracion.
+
+El cliente debe estar registrado y puede realizar la compra de varios productos. Para esto se debe 
+manejar empleados de la tienda que se encargarán de registrar las ventas.
+
+Al registrar una venta, el programa debe mostrar una tabla con los productos vendidos,
+cantidad por producto, precio unitario y precio total y la suma total de la venta.
+
+*/
